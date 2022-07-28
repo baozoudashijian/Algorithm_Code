2581fa74a80c61ef8f1996479a8bb22a19ee5a96
Roger Zhang <1787009521@qq.com>
2022-07-28 08:09:14 +0800

    feat: 自己用人类思维实现的斐波拉契

---
+++ fab/2.js
@@ -0,0 +1,12 @@
+
+// 自己使用人类思维想出的实现方法
+function fab(n) {
+    const res = [0, 1]
+    // i 
+    for(let i=0; i<n - 2; i++) {
+        res.push(res[res.length - 1] + res[res.length - 2])
+    }
+    return res[n - 1]
+}
+
+console.log(fab(45))
